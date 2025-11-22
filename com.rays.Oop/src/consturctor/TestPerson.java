21167afad9115bc4833ac6b2719e9da5f3b425be
Adhiraj1312 <tomaradhirajsingh@gmail.com>
2025-11-22 12:45:32 +0530

    done

---
+++ com.rays.Oop/src/consturctor/TestPerson.java
@@ -0,0 +1,12 @@
+package consturctor;
+
+public class TestPerson {
+
+	public static void main (String [] args) {
+		
+		Person p = new person(" Yash " + "Indore ");
+		
+		System.out.println(p.getname());
+		System.out.println(p.getaddress());
+	}
+}
---
+++ com.rays.Oop/src/consturctor/person.java
@@ -0,0 +1,17 @@
+package consturctor;
+
+public class person {
+
+	private String name ;
+	private String address;
+	
+	public person(String name , String address) {
+		this .name = name ;
+		this.address = address;
+	}
+
+	public person(String string) {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
